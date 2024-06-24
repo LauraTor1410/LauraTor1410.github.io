6d050ce69b52016cc7eb2114cba1db219f769471
Laura <laurashauny@gmail.com>
2024-06-24 13:22:52 +0200

    4 round

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "https://LauraTor1410.github.io"
+  //base: "https://LauraTor1410.github.io"
 })
